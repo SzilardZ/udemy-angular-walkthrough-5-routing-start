f449ee0aafefbee1ea31b86d9b7d47f0690086d2
SzilardZ <szilard.zr@gmail.com>
2019-04-24 21:54:58 +0200

    server component working with getting parameters from url routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,8 +19,9 @@ const appRoutes: Routes = [
   {path: 'users', component: UsersComponent},
   {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component: ServersComponent},
-  {path: 'nothing', component: NothingComponent},
+  {path: 'servers/:id', component: ServerComponent},
   {path: 'servers/:id/edit', component: EditServerComponent},
+  {path: 'nothing', component: NothingComponent},
 ];
 
 @NgModule({
