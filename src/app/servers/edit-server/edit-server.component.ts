2fae63c2522d17a27b6355245487a740e81fd9ad
SzilardZ <szilard.zr@gmail.com>
2019-04-28 20:39:44 +0200

    handling query params in server / edit-server components

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private root: ActivatedRoute) { }
@@ -19,7 +20,12 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log(this.root.snapshot.queryParams);
     console.log(this.root.snapshot.fragment);
-    this.root.queryParams.subscribe();
+    this.root.queryParams
+      .subscribe(
+        (queryParams: Params) => {
+          this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+        }
+      );
     this.root.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
